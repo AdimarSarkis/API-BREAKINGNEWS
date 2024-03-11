4f57c9540679d45045d354e29d14c3639bba621a
AdimarSarkis <sarkis170@gmail.com>
2024-03-11 20:46:42 -0300

    refactor: added the software architecture

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./src/routes/user.route")
 
-app.get("/", function (req, res) {
-    res.send("Hello World");
-})
+// ROTA
+    // Method HTTP - CRUD(CREATE - post, READ - get, UPDATE - put/patch, DELETE)
+        // GET: Pega um info
+        // POST: Cria uma info
+        // PUT: Altera toda a info
+        // PATCH: Altera parte da info
+        // DELETE: Apaga uma info
 
+    // Name - Um identificador da rota
+    // Function (Callback) - Responsável por executar algum comando 
+/* app.get("/", (req, res) => {
+    const soma = 1 + 7;
+    //res.json(soma)
+    res.send({soma: soma});
+})*/
+
+app.use("/soma", userRoute)
 app.listen(3000);
